60dfb728ed1560ccd75c575f6954768e85f5f9db
ssntosh <santosh.aryan9@gmail.com>
2015-10-20 03:02:03 -0400

    changed at  maincontrolller
    To make the service run, we need to set the same arguments order for all
    controllers, modules ,factories etc.

--- main.js
+++ main.js
@@ -1,58 +1,51 @@
-// Code goes here
-
 (function() {
-  
- var app = angular.module("main", []);
-  var maincontroller = function($scope, github, $interval, $log, $location, $anchorScroll) {
 
-    var onusercomplete = function(response) {
-      $scope.user = response.data;
-    
-    $http.get($scope.user.repos_url)
-            .then(onRepos,onError);
-            $location.hash("userdetails");
-            $anchorScroll();
-            
+  var app = angular.module("main", []);
+  var maincontroller = function($scope, $http, $interval, $log, $location, $anchorScroll, github) {
+
+    var onusercomplete = function(data) {
+      $scope.user = data;
+
+      github.getrepos($scope.user)
+        .then(onRepos, onError);
+      $location.hash("userdetails");
+      $anchorScroll();
+
     };
-    
-    var decrementcountdown = function()
-    {
-     $scope.countdown-=1;
-     if($scope.countdown<1)
-     {
-       $scope.search($scope.username);
-       
-     }
-     
-      
+
+    var decrementcountdown = function() {
+      $scope.countdown -= 1;
+      if ($scope.countdown < 1) {
+        $scope.search($scope.username);
+
+      }
+
+
     };
-  
+
     var countdownInterval = null;
-  var startcountdown = function()
-  {
-    countdownInterval = $interval(decrementcountdown,1000,$scope.countdown);
-    
-    
-  };
-    
-    var onRepos = function(response)
-    {
-      $scope.Rep = response.data;
-      
-      
+    var startcountdown = function() {
+      countdownInterval = $interval(decrementcountdown, 1000, $scope.countdown);
+
+
     };
-    
-    $scope.search = function(username)
-    {
+
+    var onRepos = function(data) {
+      $scope.Rep = data;
+
+
+    };
+
+    $scope.search = function(username) {
       $log.info("searching for " + username);
-        github.getuser(username).then(onusercomplete, onError);
-     if(countdownInterval)
-     {
-       $interval.cancel(countdownInterval);
-       $scope.countdown= null;
-     }
-      
-      
+      username=username===undefined?'angular':username;
+      github.getuser(username).then(onusercomplete, onError);
+      if (countdownInterval) {
+        $interval.cancel(countdownInterval);
+        $scope.countdown = null;
+      }
+
+
     };
     var onError = function(reason) {
       $scope.error1 = "Error accured";
@@ -60,10 +53,10 @@
 
     };
 
-    $scope.countdown="10";
-  startcountdown();
+    $scope.countdown = "10";
+    startcountdown();
 
   };
-      app.controller("maincontroller", ["$scope","$http","$interval","$log","$location","$anchorScroll","github",maincontroller]);
+  app.controller("maincontroller", ["$scope", "$http", "$interval", "$log", "$location", "$anchorScroll", "github", maincontroller]);
 
 }());
